791f8b2abc4f5b4025199882285eea705fabc7c9
Anky <1019262183@qq.com>
2026-02-03 12:08:53 +0800

    首次更新

---
+++ config.js
@@ -0,0 +1,17 @@
+export const DEFAULT_PLAYLIST_HINT = 'https://sub.ottiptv.cc/huyayqk.m3u';
+export const DEFAULT_PLAYER_BASE = 'https://tesla-kit.com/player#/?url={url}&isLive=true';
+
+// 503：服务维护/排队
+export const ENABLE_503_PAGE = false;
+export const PAGE_503_PATH = '503.html';
+
+// 501：功能即将上线
+export const ENABLE_501_PAGE = false;
+export const PAGE_501_PATH = '501.html';
+
+export const LINE_OPTIONS = [
+  { name: 'huyayqk', url: 'https://sub.ottiptv.cc/huyayqk.m3u' },
+  // { name: 'douyuyqk', url: 'https://sub.ottiptv.cc/douyuyqk.m3u' },
+  // { name: 'yylunbo', url: 'https://sub.ottiptv.cc/yylunbo.m3u' },
+  { name: 'bililive', url: 'https://sub.ottiptv.cc/bililive.m3u' }
+];
